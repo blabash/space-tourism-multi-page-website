5527f640a320b6e28ff25e8ac0d9c3ee0eef8b2e
Brett Labash <brett.labash@gmail.com>
2022-08-13 18:27:15 -0700

    Begin adding page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>SPACE TOURISM</h1>
-      <p>check it</p>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
