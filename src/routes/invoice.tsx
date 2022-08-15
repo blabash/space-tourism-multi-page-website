0ebc276962f06347672635bab1147b68f99e70ed
Brett Labash <brett.labash@gmail.com>
2022-08-14 21:52:57 -0700

    Add programmatic navigate

--- src/routes/invoice.tsx
+++ src/routes/invoice.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { getInvoice } from "../data";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { getInvoice, deleteInvoice } from "../data";
 
 type Props = {};
 type InvoiceParams = {
@@ -8,15 +8,29 @@ type InvoiceParams = {
 };
 
 export default function Invoice({}: Props) {
+  let navigate = useNavigate();
+  let location = useLocation();
   let { invoiceId } = useParams<InvoiceParams>();
   let invoice = getInvoice(Number(invoiceId));
-  return (
-    <main style={{ padding: "1em" }}>
-      <h2>Total Due: {invoice?.amount}</h2>
-      <p>
-        {invoice?.name}: {invoice?.number}
-      </p>
-      <p>Due Date: {invoice?.due}</p>
-    </main>
-  );
+  if (invoice) {
+    return (
+      <main style={{ padding: "1em" }}>
+        <h2>Total Due: {invoice.amount}</h2>
+        <p>
+          {invoice.name}: {invoice.number}
+        </p>
+        <p>Due Date: {invoice.due}</p>
+        <p>
+          <button
+            onClick={() => {
+              invoice && deleteInvoice(invoice.number);
+              navigate("/invoices" + location.search);
+            }}
+          >
+            Delete
+          </button>
+        </p>
+      </main>
+    );
+  } else return <></>;
 }
